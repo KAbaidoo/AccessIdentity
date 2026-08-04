ec746ab7421eaf75f60801d8ffb82ee7fcfcb898
Kelvin Abaidoo <kabaidoo@hotmail.com>
2026-08-04 22:23:30 +0000

    feat(vaultpass): initialize VaultPass app with SharedSecurityKit dependency
    - Create initial iOS SwiftUI project for VaultPass
    
    - Configure Xcode project to import and link SharedSecurityKit SPM package

---
+++ VaultPass/VaultPass/VaultPassApp.swift
@@ -0,0 +1,17 @@
+//
+//  VaultPassApp.swift
+//  VaultPass
+//
+//  Created by kobby on 04/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct VaultPassApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
